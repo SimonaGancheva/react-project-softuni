aef0554bd6ac084d67fb83ee2a7944838f085385
SimonaGancheva <ilievas95@gmail.com>
2023-12-07 07:38:34 +0200

    Show user's events in the profile page

--- client/src/components/UserProfile/UserProfile.js
+++ client/src/components/UserProfile/UserProfile.js
@@ -1,5 +1,27 @@
+import { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../../contexts/AuthContext';
+
+import { EventContext } from '../../contexts/EventContext';
+import { EventCard } from '../EventCard/EventCard';
+
 export const UserProfile = () => {
-    return (
-        <div>this is the profile page</div>
-    )
-}
+  const { userId } = useContext(AuthContext);
+  const { events } = useContext(EventContext);
+  const [userEvents, setUserEvents] = useState([]);
+
+  useEffect(() => {
+    const currUserEvents = events.filter((x) => x._ownerId === userId);
+    setUserEvents(currUserEvents);
+  }, [events]);
+
+  return (
+    <>
+      <div>this is the profile page</div>
+      <div className="row">
+        {userEvents.map((x) => (
+          <EventCard key={x._id} {...x} />
+        ))}
+      </div>
+    </>
+  );
+};
